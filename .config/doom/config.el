c8065e6dec2f7162c297b93da2e92780cf217348
Drew Simkins <andrewsimk@gmail.com>
2020-09-27 15:06:23 +0100

    Change theming and font of Doom

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -3,6 +3,9 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(setq doom-font (font-spec :family "Lekton Nerd Font" :size 18)
+       doom-variable-pitch-font (font-spec :family "Lekton Nerd Font" :size 18))
+
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
@@ -25,11 +28,13 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+
+(setq doom-theme 'doom-horizon)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Doc/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
